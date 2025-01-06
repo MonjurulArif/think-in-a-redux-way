f43c8d3aa4fec214c6e0b9219294a0e986aa3894
MonjurulArif <monjurularif@gmail.com>
2025-01-06 23:03:35 +0600

    React State Management issues

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,29 +1,14 @@
-import React, { useState } from "react";
+import Button from "./Button";
+import Count from "./Count";
 
-export default function Counter() {
-	const [count, setCount] = useState(0);
-
-	const increment = () => {
-		setCount((prevCount) => prevCount + 1);
-	};
-
-	const decrement = () => {
-		setCount((prevCount) => prevCount - 1);
-	};
-
+export default function Counter({ id, count, increment, decrement }) {
 	return (
 		<div>
 			<div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-				<div className="text-2xl font-semibold" id="counter">
-					{count}
-				</div>
+				<Count count={count}></Count>
 				<div className="flex space-x-3">
-					<button className="bg-indigo-400 text-white px-3 py-2 rounded shadow" id="increment" onClick={increment}>
-						Increment
-					</button>
-					<button className="bg-red-400 text-white px-3 py-2 rounded shadow" id="decrement" onClick={decrement}>
-						Decrement
-					</button>
+					<Button handler={() => increment(id)}>Increment</Button>
+					<Button handler={() => decrement(id)}>Decrement</Button>
 				</div>
 			</div>
 		</div>
